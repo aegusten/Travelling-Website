9fd6e315cc71075623132aac040ba87d00b29a14
Yan <aegustenmail@gmail.com>
2025-03-26 00:17:02 +0800

    New

--- backend/settings.py
+++ backend/settings.py
@@ -62,6 +62,9 @@ DATABASES = {
     }
 }
 
+SESSION_ENGINE = "django.contrib.sessions.backends.db"
+
+
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
     {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
